6b7e8661101d1adb2e87619e7fcdd19d6248f466
toybii03 <johnbenjie008@gmail.com>
2025-05-27 14:22:24 +0800

    Update App.tsx in line 60

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -57,7 +57,7 @@ function App() {
           }
         />
         <Route
-          path="/feedback"
+          path="/feedback-analytics"
           element={
             <RequireAuth>
               <FeedbackAnalytics />
